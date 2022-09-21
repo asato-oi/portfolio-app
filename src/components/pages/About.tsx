ee7c51ccd6c3570b1e75ff155b0260c3e937588d
AsatoOi <83337845+AsatoOi@users.noreply.github.com>
2022-09-22 00:40:26 +0900

    ✅

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -28,8 +28,9 @@ export const About: FC = memo(() => {
       <Heading>
         ASATO OI <span style={{ display: "inline-block" }}>｜ 大井 朝登</span>
       </Heading>
-      <P2>多摩美術大学 美術学部 演劇舞踊デザイン学科　卒</P2>
-      <P2>日本電子専門学校 Webデザイン科　在学</P2>
+      <P2>Tama Art University </P2>
+      <P2>Japan Electronics College Web Design Department</P2>
+      <P2>University of London　Computer Science　</P2>
       <P1>
         高校時代に観た演劇「ハムレット」に衝撃を受け、大学で舞台芸術を学ぶことを決意する。多摩美術大学で演劇を学ぶ中で、演劇に留まらず、アート表現の本質は「人々の心を動かして世の中の人々を幸せにすることではないか」と思うようになる。4年間でディレクションや演出を中心に勉強し、卒業制作公演はディレクターを受け持つ。就職活動の一方、自らモノを作り世の中の人々を幸せにしたいという思いが捨てきれず、父の勧めもありプログラミングに興味をもつ。きちんと学びたいという思いから日本電子専門学校への進学をきめ、現在はフロントエンジニアとして人々を幸せにできるコンテンツを模索、制作を行っている。
       </P1>
