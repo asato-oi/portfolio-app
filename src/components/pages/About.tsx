1ab56c763408bd999660bbd596464dae2e0092fc
AsatoOi <83337845+AsatoOi@users.noreply.github.com>
2022-09-22 15:28:43 +0900

    💄

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -16,7 +16,7 @@ export const About: FC = memo(() => {
   return (
     <Inner
       style={{
-        maxWidth: 900,
+        maxWidth: 850,
         paddingTop: space.l,
         paddingBottom: space.xl,
       }}
@@ -31,22 +31,36 @@ export const About: FC = memo(() => {
       <P2>Tama Art University </P2>
       <P2>Japan Electronics College Web Design Department</P2>
       <P2>University of London　Computer Science　</P2>
-      <P1>
-        高校時代に観た演劇「ハムレット」に衝撃を受け、大学で舞台芸術を学ぶことを決意する。多摩美術大学で演劇を学ぶ中で、演劇に留まらず、アート表現の本質は「人々の心を動かして世の中の人々を幸せにすることではないか」と思うようになる。4年間でディレクションや演出を中心に勉強し、卒業制作公演はディレクターを受け持つ。就職活動の一方、自らモノを作り世の中の人々を幸せにしたいという思いが捨てきれず、父の勧めもありプログラミングに興味をもつ。きちんと学びたいという思いから日本電子専門学校への進学をきめ、現在はフロントエンジニアとして人々を幸せにできるコンテンツを模索、制作を行っている。
-      </P1>
+      <div style={{ marginTop: space.xl, marginBottom: space.xl }}>
+        <P1 style={{ position: "relative" }}>
+          <P2>A word I like</P2>
+          <blockquote>
+            <p>
+              &emsp;All you need in this life is ignorance and confidence, and
+              then success is sure.
+            </p>
+          </blockquote>
+          <p style={{ position: "absolute", right: 25 }}>
+            {" "}
+            &emsp;-- Mark Twain
+          </p>
+        </P1>
+      </div>
+
+      {/* 高校時代に観た演劇「ハムレット」に衝撃を受け、大学で舞台芸術を学ぶことを決意する。多摩美術大学で演劇を学ぶ中で、演劇に留まらず、アート表現の本質は「人々の心を動かして世の中の人々を幸せにすることではないか」と思うようになる。4年間でディレクションや演出を中心に勉強し、卒業制作公演はディレクターを受け持つ。就職活動の一方、自らモノを作り世の中の人々を幸せにしたいという思いが捨てきれず、父の勧めもありプログラミングに興味をもつ。きちんと学びたいという思いから日本電子専門学校への進学をきめ、現在はフロントエンジニアとして人々を幸せにできるコンテンツを模索、制作を行っている。 */}
       <br />
       <div style={{ display: "flex", justifyContent: "flex-start" }}>
         <div>
-          <P2>好きなこと</P2>
+          <P2>Favorite things</P2>
           <Ul>
-            <Li>プログラミングを勉強すること</Li>
-            <Li>子供達と触れ合い、勉強を教えること</Li>
-            <Li>愛犬のコムギと散歩をすること</Li>
-            <Li>映画を見ること</Li>
+            <Li>programming instruction</Li>
+            <Li>to teach my clam school children English and math</Li>
+            <Li>Walking with my dog</Li>
+            <Li>Watching movies</Li>
           </Ul>
         </div>
         <div style={{ marginLeft: space.xl }}>
-          <P2>次なる挑戦</P2>
+          <P2>Next challenges</P2>
           <Ul>
             <Li>Vue</Li>
             <Li>NEXT.js</Li>
@@ -62,6 +76,7 @@ export const About: FC = memo(() => {
 
 const Heading = styled("h2")({
   color: theme.theme,
+  marginBottom: space.l,
   fontSize: fontSize.xxxxl,
   fontWeight: fontWeight.bold,
   lineHeight: lineHeight.heading,
