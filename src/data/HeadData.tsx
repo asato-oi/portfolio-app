12b9921d6ad751e82c5b766028dc08642f3002f5
AsatoOi <83337845+AsatoOi@users.noreply.github.com>
2022-09-29 11:28:06 +0900

    headの更新

--- src/data/HeadData.tsx
+++ src/data/HeadData.tsx
@@ -5,8 +5,8 @@ import { Helmet } from "react-helmet";
 export const HeadData: FC = memo(() => {
   return (
     <Helmet>
-      <title>ASATO OI Portfolio | Work</title>
       <meta charSet="utf-8" />
+      <title>ASATO OI Portfolio</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta
         name="description"
@@ -20,11 +20,14 @@ export const HeadData: FC = memo(() => {
       <meta property="og:title" content="ASATO OI Portfolio|Work" />
       <meta
         property="og:description"
-        content="ASATO OI｜大井朝登のポートフォリオサイト"
+        content="大井朝登のポートフォリオサイト"
       />
       <meta property="og:site_name" content="ASATO OI Portfolio" />
       <meta property="og:locale" content="ja_JP" />
-      <meta name="keywords" content="ASATO, OI, 大井, 朝登" />
+      <meta
+        name="keywords"
+        content="ASATO,OI,大井,朝登,おおい,あさと,オオイ,アサト,大井朝登,おおいあさと,AsatoOi,オオイアサト,大井 朝登,おおい あさと,オオイ アサト,Asato Oi,ASATO OI,ASATOOI"
+      />
       <meta name="theme-color" content="#000000" />
     </Helmet>
   );
